cb9bcf73b2e8a626d82722741cb412527e6d7385
Ethan Mercer <emercer3@byu.edu>
2025-03-25 19:43:34 +0000

    board prints fully

--- client/src/main/java/ui/BoardPrint.java
+++ client/src/main/java/ui/BoardPrint.java
@@ -5,12 +5,23 @@ import static ui.EscapeSequences.*;
 public class BoardPrint {
   
   public static void drawBoard(String color) {
-    int previous = 1;
+    int previous = -1;
+    int redPawn = 2;
+    int greyPawn = 7;
+    int redPieces = 1;
+    int greyPieces = 8;
     int[] numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    reverseArray(numbers);
     String[] letters = {"", "a", "b", "c", "d", "e", "f", "g", "h"};
+    String[] pieces = {"", "r", "k", "b", "q", "k", "b", "k", "r"};
     
 
     if (!color.equals("white")) {
+      redPawn = 7;
+      greyPawn = 2;
+      redPieces = 8;
+      greyPieces = 1;
+      previous = 1;
       reverseArray(numbers);
       reverseArray(letters);
   }
@@ -38,10 +49,30 @@ public class BoardPrint {
             System.out.print(SET_BG_COLOR_MAGENTA + "   " + RESET_BG_COLOR);
           }
         } else if (previous == 1) {
-          System.out.print(SET_BG_COLOR_WHITE + "   " + RESET_BG_COLOR);
+          if (i == greyPawn) {
+            System.out.print(SET_BG_COLOR_WHITE + SET_TEXT_COLOR_LIGHT_GREY + " " + "p" + " " + RESET_TEXT_COLOR + RESET_BG_COLOR);
+          } else if (i == redPawn) {
+            System.out.print(SET_BG_COLOR_WHITE + SET_TEXT_COLOR_RED + " " + "p" + " " + RESET_TEXT_COLOR + RESET_BG_COLOR);
+          } else if (i == greyPieces) {
+            System.out.print(SET_BG_COLOR_WHITE + SET_TEXT_COLOR_LIGHT_GREY + " " + pieces[j] + " " + RESET_TEXT_COLOR + RESET_BG_COLOR);
+          } else if (i == redPieces) {
+            System.out.print(SET_BG_COLOR_WHITE + SET_TEXT_COLOR_RED + " " + pieces[j] + " " + RESET_TEXT_COLOR + RESET_BG_COLOR);
+          } else {
+            System.out.print(SET_BG_COLOR_WHITE + "   " + RESET_BG_COLOR);
+          }
           previous = -1;
         } else {
-          System.out.print(SET_BG_COLOR_BLACK + "   " + RESET_BG_COLOR);
+          if (i == greyPawn) {
+            System.out.print(SET_BG_COLOR_BLACK + SET_TEXT_COLOR_LIGHT_GREY + " " + "p" + " " + RESET_TEXT_COLOR + RESET_BG_COLOR);
+          } else if (i == redPawn) {
+            System.out.print(SET_BG_COLOR_BLACK + SET_TEXT_COLOR_RED + " " + "p" + " " + RESET_TEXT_COLOR + RESET_BG_COLOR);
+          } else if (i == greyPieces) {
+            System.out.print(SET_BG_COLOR_BLACK + SET_TEXT_COLOR_LIGHT_GREY + " " + pieces[j] + " " + RESET_TEXT_COLOR + RESET_BG_COLOR);
+          } else if (i == redPieces) {
+            System.out.print(SET_BG_COLOR_BLACK + SET_TEXT_COLOR_RED + " " + pieces[j] + " " + RESET_TEXT_COLOR + RESET_BG_COLOR);
+          } else {
+            System.out.print(SET_BG_COLOR_BLACK + "   " + RESET_BG_COLOR);
+          }
           previous = 1;
         }
       }
