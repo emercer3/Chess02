2dfdd0e11a83cc32a9e58544a4fc8a30340171cb
Ethan Mercer <emercer3@byu.edu>
2025-04-04 22:19:53 +0000

    Add functions to websocketfacade(non-tested), next is client stuff

--- client/src/main/java/websocket/WebSocketFacade.java
+++ client/src/main/java/websocket/WebSocketFacade.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import exception.ResponseException;
 import websocket.commands.*;
 import websocket.messages.*;
+import chess.*;
 
 import javax.websocket.*;
 import java.io.IOException;
@@ -39,12 +40,41 @@ public class WebSocketFacade extends Endpoint {
   public void onOpen(Session arg0, EndpointConfig arg1) {
   }
 
-  public void joinGame() throws ResponseException {
-
+  public void joinGame(String authToken, int gameId) throws ResponseException {
+    try {
+      var action = new UserGameCommand(UserGameCommand.CommandType.CONNECT, authToken, gameId, null);
+      this.session.getBasicRemote().sendText(new Gson().toJson(action));
+    } catch (IOException e) {
+      throw new ResponseException(500, e.getMessage());
+    }
   }
 
-  public void leaveGame() throws ResponseException {
-    
+  public void makeMove(String authToken, int gameId, ChessMove move) throws ResponseException {
+    try {
+      var action = new UserGameCommand(UserGameCommand.CommandType.MAKE_MOVE, authToken, gameId, move);
+      this.session.getBasicRemote().sendText(new Gson().toJson(action));
+    } catch (IOException e) {
+      throw new ResponseException(500, e.getMessage());
+    }
+  }
+
+  public void resign(String authToken, int gameId) throws ResponseException {
+    try {
+      var action = new UserGameCommand(UserGameCommand.CommandType.RESIGN, authToken, gameId, null);
+      this.session.getBasicRemote().sendText(new Gson().toJson(action));
+    } catch (IOException e) {
+      throw new ResponseException(500, e.getMessage());
+    }
+  }
+
+  public void leaveGame(String authToken, int gameId) throws ResponseException {
+    try {
+      var action = new UserGameCommand(UserGameCommand.CommandType.LEAVE, authToken, gameId, null);
+      this.session.getBasicRemote().sendText(new Gson().toJson(action));
+      this.session.close();
+    } catch (IOException e) {
+      throw new ResponseException(500, e.getMessage());
+    }
   }
   
 }
