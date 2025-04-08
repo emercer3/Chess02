50196dfedbd07097fd88c598f3f93b1a5dd61ddd
Ethan Mercer <emercer3@byu.edu>
2025-04-08 03:55:03 +0000

    removed and fixed duplicated blocks in boardprint

--- client/src/main/java/ui/BoardPrint.java
+++ client/src/main/java/ui/BoardPrint.java
@@ -9,26 +9,27 @@ import java.util.Collection;
 import chess.*;
 
 public class BoardPrint {
+  private static int[] numbers = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+  private static String[] letters = { "", "a", "b", "c", "d", "e", "f", "g", "h", "" };
+  private static Map<ChessPiece.PieceType, String> piecesMap = Map.of(
+    ChessPiece.PieceType.PAWN, "p",
+    ChessPiece.PieceType.KNIGHT, "n",
+    ChessPiece.PieceType.BISHOP, "b",
+    ChessPiece.PieceType.ROOK, "r",
+    ChessPiece.PieceType.QUEEN, "q",
+    ChessPiece.PieceType.KING, "k");
+  private static Map<ChessGame.TeamColor, String> colorMap = Map.of(
+      ChessGame.TeamColor.WHITE, SET_TEXT_COLOR_RED,
+      ChessGame.TeamColor.BLACK, SET_TEXT_COLOR_LIGHT_GREY);
+  private static Collection<ChessMove> validMoves = new ArrayList<>();
+
+  
 
   public static void drawBoard(String color, ChessGame game, ChessPosition highlight) {
     int previous = -1;
 
-    int[] numbers = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
-    reverseArray(numbers);
-    String[] letters = { "", "a", "b", "c", "d", "e", "f", "g", "h", "" };
-
     ChessBoard board = game.getBoard().copy();
-    Map<ChessPiece.PieceType, String> piecesMap = Map.of(
-        ChessPiece.PieceType.PAWN, "p",
-        ChessPiece.PieceType.KNIGHT, "n",
-        ChessPiece.PieceType.BISHOP, "b",
-        ChessPiece.PieceType.ROOK, "r",
-        ChessPiece.PieceType.QUEEN, "q",
-        ChessPiece.PieceType.KING, "k");
-    Map<ChessGame.TeamColor, String> colorMap = Map.of(
-        ChessGame.TeamColor.WHITE, SET_TEXT_COLOR_RED,
-        ChessGame.TeamColor.BLACK, SET_TEXT_COLOR_LIGHT_GREY);
-    Collection<ChessMove> validMoves = new ArrayList<>();
+    int[] localNumbers = numbers.clone();
 
     if (highlight != null) {
       Collection<ChessMove> moves = new ArrayList<>();
@@ -36,10 +37,8 @@ public class BoardPrint {
     }
 
     if (!color.equals("WHITE")) {
-      // board = flipBoard(board);
-      // reverseArray(numbers);
-      // reverseArray(letters);
-      drawblack(letters, numbers, previous, board, highlight, piecesMap, colorMap, game, validMoves);
+      reverseArray(localNumbers);
+      drawblack(previous, board, highlight, game, localNumbers);
       return;
     }
 
@@ -47,70 +46,20 @@ public class BoardPrint {
 
     for (int i = 9; i >= 0; i--) {
       for (int j = 0; j < 10; j++) {
-        if (i == 0 && j == 0) {
-          System.out.print("   ");
-        } else if (i == 0 && j == 9) {
-          System.out.print("   ");
-        } else if (i == 9 && j == 0) {
-          System.out.print("   ");
-        } else if (i == 9 && j == 9) {
-          System.out.print("   ");
-        } else if (i == 0 || i == 9) {
-          if (j > 0 && j < 9) {
-            System.out.print(SET_BG_COLOR_MAGENTA + SET_TEXT_COLOR_BLACK + " " + letters[j] + " " + RESET_TEXT_COLOR
-                + RESET_BG_COLOR);
-          } else {
-            System.out.print(SET_BG_COLOR_MAGENTA + "   " + RESET_BG_COLOR);
-          }
-        } else if (j == 0 || j == 9) {
-          if (i > 0 && i < 9) {
-            System.out.print(SET_BG_COLOR_MAGENTA + SET_TEXT_COLOR_BLACK + " " + numbers[i] + " " + RESET_TEXT_COLOR
-                + RESET_BG_COLOR);
-          } else {
-            System.out.print(SET_BG_COLOR_MAGENTA + "   " + RESET_BG_COLOR);
-          }
-        } else if (previous == 1) {
+        ChessPosition position = new ChessPosition(i, j);
+        ChessMove validMove = new ChessMove(highlight, position, null);
+
+        if (i == 0 || i == 9 || j == 0 || j == 9) {
+          margins(i, j);
+        }
+        else if (previous == 1) {
           previous = -1;
-          ChessPosition position = new ChessPosition(i, j);
-          ChessMove validMove = new ChessMove(highlight, position, null);
-
-          if (board.getPiece(position) == null) {
-            if (highlight != null && validMoves.contains(validMove)) {
-              System.out.print(SET_BG_COLOR_YELLOW + "   " + RESET_BG_COLOR);
-            } else {
-              System.out.print(SET_BG_COLOR_WHITE + "   " + RESET_BG_COLOR);
-            }
-          } else {
-            if (validMoves.contains(validMove)) {
-              System.out.print(SET_BG_COLOR_YELLOW + colorMap.get(board.getPiece(position).getTeamColor()) + " "
-                  + piecesMap.get(board.getPiece(position).getPieceType()) + " " + RESET_TEXT_COLOR + RESET_BG_COLOR);
-            } else {
-              System.out.print(SET_BG_COLOR_WHITE + colorMap.get(board.getPiece(position).getTeamColor()) + " "
-                  + piecesMap.get(board.getPiece(position).getPieceType()) + " " + RESET_TEXT_COLOR + RESET_BG_COLOR);
-            }
-          }
-
-        } else {
+          white(i, j, board, position, highlight, validMove);
+        } 
+        
+        else {
           previous = 1;
-          ChessPosition position = new ChessPosition(i, j);
-          ChessMove validMove = new ChessMove(highlight, position, null);
-
-          if (board.getPiece(position) == null) {
-            if (highlight != null && validMoves.contains(validMove)) {
-              System.out.print(SET_BG_COLOR_YELLOW + "   " + RESET_BG_COLOR);
-            } else {
-              System.out.print(SET_BG_COLOR_BLACK + "   " + RESET_BG_COLOR);
-            }
-          } else {
-            if (validMoves.contains(validMove)) {
-              System.out.print(SET_BG_COLOR_YELLOW + colorMap.get(board.getPiece(position).getTeamColor()) + " "
-                  + piecesMap.get(board.getPiece(position).getPieceType()) + " " + RESET_TEXT_COLOR + RESET_BG_COLOR);
-            } else {
-              System.out.print(SET_BG_COLOR_BLACK + colorMap.get(board.getPiece(position).getTeamColor()) + " "
-                  + piecesMap.get(board.getPiece(position).getPieceType()) + " " + RESET_TEXT_COLOR + RESET_BG_COLOR);
-            }
-          }
-
+          black(i, j, board, position, highlight, validMove);
         }
 
       }
@@ -119,119 +68,102 @@ public class BoardPrint {
     }
   }
 
-  public static void drawblack(
-      String[] letters,
-      int[] numbers,
-      int previous,
-      ChessBoard board,
-      ChessPosition highlight,
-      Map<ChessPiece.PieceType, String> piecesMap,
-      Map<ChessGame.TeamColor, String> colorMap,
-      ChessGame game,
-      Collection<ChessMove> validMoves) {
+
+  public static void margins(int i, int j) {
+    if (i == 0 && j == 0) {
+      System.out.print("   ");
+    } else if (i == 0 && j == 9) {
+      System.out.print("   ");
+    } else if (i == 9 && j == 0) {
+      System.out.print("   ");
+    } else if (i == 9 && j == 9) {
+      System.out.print("   ");
+    } else if (i == 0 || i == 9) {
+      if (j > 0 && j < 9) {
+        System.out.print(SET_BG_COLOR_MAGENTA + SET_TEXT_COLOR_BLACK + " " + letters[j] + " " + RESET_TEXT_COLOR
+            + RESET_BG_COLOR);
+      } else {
+        System.out.print(SET_BG_COLOR_MAGENTA + "   " + RESET_BG_COLOR);
+      }
+    } else if (j == 0 || j == 9) {
+      if (i > 0 && i < 9) {
+        System.out.print(SET_BG_COLOR_MAGENTA + SET_TEXT_COLOR_BLACK + " " + numbers[i] + " " + RESET_TEXT_COLOR
+            + RESET_BG_COLOR);
+      } else {
+        System.out.print(SET_BG_COLOR_MAGENTA + "   " + RESET_BG_COLOR);
+      }
+    }
+  }
+
+  public static void white(int i, int j, ChessBoard board, ChessPosition position, ChessPosition highlight, ChessMove validMove) {
+    if (board.getPiece(position) == null) {
+      if (highlight != null && validMoves.contains(validMove)) {
+        System.out.print(SET_BG_COLOR_YELLOW + "   " + RESET_BG_COLOR);
+      } else {
+        System.out.print(SET_BG_COLOR_WHITE + "   " + RESET_BG_COLOR);
+      }
+    } else {
+      if (validMoves.contains(validMove)) {
+        System.out.print(SET_BG_COLOR_YELLOW + colorMap.get(board.getPiece(position).getTeamColor()) + " "
+            + piecesMap.get(board.getPiece(position).getPieceType()) + " " + RESET_TEXT_COLOR + RESET_BG_COLOR);
+      } else {
+        System.out.print(SET_BG_COLOR_WHITE + colorMap.get(board.getPiece(position).getTeamColor()) + " "
+            + piecesMap.get(board.getPiece(position).getPieceType()) + " " + RESET_TEXT_COLOR + RESET_BG_COLOR);
+      }
+    }
+  }
+
+  public static void black(int i, int j, ChessBoard board, ChessPosition position, ChessPosition highlight, ChessMove validMove) {
+    if (board.getPiece(position) == null) {
+      if (highlight != null && validMoves.contains(validMove)) {
+        System.out.print(SET_BG_COLOR_YELLOW + "   " + RESET_BG_COLOR);
+      } else {
+        System.out.print(SET_BG_COLOR_BLACK + "   " + RESET_BG_COLOR);
+      }
+    } else {
+      if (validMoves.contains(validMove)) {
+        System.out.print(SET_BG_COLOR_YELLOW + colorMap.get(board.getPiece(position).getTeamColor()) + " "
+            + piecesMap.get(board.getPiece(position).getPieceType()) + " " + RESET_TEXT_COLOR + RESET_BG_COLOR);
+      } else {
+        System.out.print(SET_BG_COLOR_BLACK + colorMap.get(board.getPiece(position).getTeamColor()) + " "
+            + piecesMap.get(board.getPiece(position).getPieceType()) + " " + RESET_TEXT_COLOR + RESET_BG_COLOR);
+      }
+    }
+  }
+
+
+  public static void drawblack(int previous, ChessBoard board, ChessPosition highlight, ChessGame game, int[] numbers) {
+
     System.out.print("\n");
 
     for (int i = 0; i < 10; i++) {
       for (int j = 9; j >= 0; j--) {
-        if (i == 0 && j == 0) {
-          System.out.print("   ");
-        } else if (i == 0 && j == 9) {
-          System.out.print("   ");
-        } else if (i == 9 && j == 0) {
-          System.out.print("   ");
-        } else if (i == 9 && j == 9) {
-          System.out.print("   ");
-        } else if (i == 0 || i == 9) {
-          if (j > 0 && j < 9) {
-            System.out.print(SET_BG_COLOR_MAGENTA + SET_TEXT_COLOR_BLACK + " " + letters[j] + " " + RESET_TEXT_COLOR
-                + RESET_BG_COLOR);
-          } else {
-            System.out.print(SET_BG_COLOR_MAGENTA + "   " + RESET_BG_COLOR);
-          }
-        } else if (j == 0 || j == 9) {
-          if (i > 0 && i < 9) {
-            System.out.print(SET_BG_COLOR_MAGENTA + SET_TEXT_COLOR_BLACK + " " + numbers[i] + " " + RESET_TEXT_COLOR
-                + RESET_BG_COLOR);
-          } else {
-            System.out.print(SET_BG_COLOR_MAGENTA + "   " + RESET_BG_COLOR);
-          }
-        } else if (previous == 1) {
+        ChessPosition position = new ChessPosition(i, j);
+        ChessMove validMove = new ChessMove(highlight, position, null);
+
+        if (i == 0 || i == 9 || j == 0 || j == 9) {
+          margins(i, j);
+        }
+        else if (previous == 1) {
           previous = -1;
-          ChessPosition position = new ChessPosition(i, j);
-          ChessMove validMove = new ChessMove(highlight, position, null);
-
-          if (board.getPiece(position) == null) {
-            if (highlight != null && validMoves.contains(validMove)) {
-              System.out.print(SET_BG_COLOR_YELLOW + "   " + RESET_BG_COLOR);
-            } else {
-              System.out.print(SET_BG_COLOR_WHITE + "   " + RESET_BG_COLOR);
-            }
-          } else {
-            if (validMoves.contains(validMove)) {
-              System.out.print(SET_BG_COLOR_YELLOW + colorMap.get(board.getPiece(position).getTeamColor()) + " "
-                  + piecesMap.get(board.getPiece(position).getPieceType()) + " " + RESET_TEXT_COLOR + RESET_BG_COLOR);
-            } else {
-              System.out.print(SET_BG_COLOR_WHITE + colorMap.get(board.getPiece(position).getTeamColor()) + " "
-                  + piecesMap.get(board.getPiece(position).getPieceType()) + " " + RESET_TEXT_COLOR + RESET_BG_COLOR);
-            }
-          }
-
-        } else {
+          white(i, j, board, position, highlight, validMove);
+        } 
+        
+        else {
           previous = 1;
-          ChessPosition position = new ChessPosition(i, j);
-          ChessMove validMove = new ChessMove(highlight, position, null);
-
-          if (board.getPiece(position) == null) {
-            if (highlight != null && validMoves.contains(validMove)) {
-              System.out.print(SET_BG_COLOR_YELLOW + "   " + RESET_BG_COLOR);
-            } else {
-              System.out.print(SET_BG_COLOR_BLACK + "   " + RESET_BG_COLOR);
-            }
-          } else {
-            if (validMoves.contains(validMove)) {
-              System.out.print(SET_BG_COLOR_YELLOW + colorMap.get(board.getPiece(position).getTeamColor()) + " "
-                  + piecesMap.get(board.getPiece(position).getPieceType()) + " " + RESET_TEXT_COLOR + RESET_BG_COLOR);
-            } else {
-              System.out.print(SET_BG_COLOR_BLACK + colorMap.get(board.getPiece(position).getTeamColor()) + " "
-                  + piecesMap.get(board.getPiece(position).getPieceType()) + " " + RESET_TEXT_COLOR + RESET_BG_COLOR);
-            }
-          }
-
+          black(i, j, board, position, highlight, validMove);
         }
 
       }
       System.out.println();
       previous = previous * -1;
     }
-  }
-
-  public static ChessBoard flipBoard(ChessBoard original) {
-    ChessBoard flipped = new ChessBoard();
-    for (int i = 0; i < 8; i++) {
-      for (int j = 0; j < 8; j++) {
-        ChessPiece piece = original.getPiece(new ChessPosition(i + 1, j + 1));
-        if (piece != null) {
-          // Place the piece in the flipped position
-          flipped.addPiece(new ChessPosition(8 - i, 8 - j), piece);
-        }
-      }
-    }
-    return flipped;
   }
 
   // Helper method to reverse an int array pulled from copilot
   private static void reverseArray(int[] array) {
     for (int i = 0; i < array.length / 2; i++) {
       int temp = array[i];
-      array[i] = array[array.length - 1 - i];
-      array[array.length - 1 - i] = temp;
-    }
-  }
-
-  // Helper method to reverse a String array pulled from copilot
-  private static void reverseArray(String[] array) {
-    for (int i = 0; i < array.length / 2; i++) {
-      String temp = array[i];
       array[i] = array[array.length - 1 - i];
       array[array.length - 1 - i] = temp;
     }
